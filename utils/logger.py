4e886db06da26f1c324bf9bb1f83c74f9a8afae5
meghanams002 <meghana.ms002@gmail.com>
2025-10-25 15:30:59 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,12 +1,22 @@
 import logging
+import os
 
-def get_logger(name="test_logger"):
-    logger = logging.getLogger(name)
+def setup_logger():
+    os.makedirs("logs", exist_ok=True)
+    log_path = "logs/test.log"
+
+    logger = logging.getLogger("TestLogger")
+    logger.setLevel(logging.DEBUG)
+
+    file_handler = logging.FileHandler(log_path)
+    console_handler = logging.StreamHandler()
+
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
     if not logger.handlers:
-        handler = logging.FileHandler("logs/test.log")
-        formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-        logger.setLevel(logging.INFO)
-    return logger
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
 
+    return logger
