652498c961c5001c81c752a7259cbb1fd0e878f4
meghanams002 <meghana.ms002@gmail.com>
2025-10-25 15:31:58 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,18 @@
 import pytest
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from utils.logger import get_logger
+from utils.logger import setup_logger
 
-@pytest.fixture(scope="session")
-def logger():
-    return get_logger()
+@pytest.fixture(scope="session", autouse=True)
+def setup_logging():
+    return setup_logger()
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     yield driver
     driver.quit()
-
