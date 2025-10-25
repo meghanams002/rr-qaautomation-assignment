6f0028d8a3d70b11b0577078d09b61de99526d35
meghanams002 <meghana.ms002@gmail.com>
2025-10-25 13:11:22 +0530

    Update base_page.py

--- utils/base_page.py
+++ utils/base_page.py
@@ -1,1 +1,16 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+
+    def click(self, locator):
+        self.wait.until(EC.element_to_be_clickable(locator)).click()
+
+    def get_texts(self, locator):
+        self.wait.until(EC.presence_of_all_elements_located(locator))
+        return [el.text for el in self.driver.find_elements(*locator)]
 
