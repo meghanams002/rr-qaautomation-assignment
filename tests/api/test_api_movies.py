9b9b0491c4c8775751cc6c31e29fd23bc7f79135
meghanams002 <meghana.ms002@gmail.com>
2025-10-25 15:33:25 +0530

    Update test_api_movies.py

--- tests/api/test_api_movies.py
+++ tests/api/test_api_movies.py
@@ -1,10 +1,19 @@
+import pytest
 import requests
-from utils.config import API_URL
+from utils.config import TRENDING_API
 
-def test_api_popular_movies():
-    response = requests.get(f"{API_URL}/movie/popular")
-    assert response.status_code == 200, "Expected status 200"
+@pytest.mark.api
+def test_trending_api_response_status(setup_logging):
+    logger = setup_logging
+    logger.info("Validating Trending API status")
+    response = requests.get(TRENDING_API)
+    assert response.status_code == 200, f"Expected 200, got {response.status_code}"
+
+@pytest.mark.api
+def test_trending_api_content(setup_logging):
+    logger = setup_logging
+    logger.info("Validating Trending API response content")
+    response = requests.get(TRENDING_API)
     data = response.json()
-    assert "results" in data, "Missing results field"
-    assert isinstance(data["results"], list)
-
+    assert "results" in data, "Key 'results' missing in API response"
+    assert len(data["results"]) > 0, "No trending movies/TV shows found"
