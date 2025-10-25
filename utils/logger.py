f7488eb97237147c55e7ba4870d126a85fee2596
meghanams002 <meghana.ms002@gmail.com>
2025-10-25 13:10:53 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,12 @@
+import logging
+
+def get_logger(name="test_logger"):
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.FileHandler("logs/test.log")
+        formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+    return logger
 
