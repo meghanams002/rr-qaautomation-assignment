a64b0b15e287a3e55f55329494782ddffda06d74
meghanams002 <meghana.ms002@gmail.com>
2025-10-25 13:11:45 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,16 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from utils.logger import get_logger
+
+@pytest.fixture(scope="session")
+def logger():
+    return get_logger()
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
+    yield driver
+    driver.quit()
 
