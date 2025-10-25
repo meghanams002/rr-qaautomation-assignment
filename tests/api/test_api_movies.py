1cabd09a53f3114514107793943d099db168015e
meghanams002 <meghana.ms002@gmail.com>
2025-10-25 13:13:07 +0530

    Update test_api_movies.py

--- tests/api/test_api_movies.py
+++ tests/api/test_api_movies.py
@@ -1,1 +1,10 @@
+import requests
+from utils.config import API_URL
+
+def test_api_popular_movies():
+    response = requests.get(f"{API_URL}/movie/popular")
+    assert response.status_code == 200, "Expected status 200"
+    data = response.json()
+    assert "results" in data, "Missing results field"
+    assert isinstance(data["results"], list)
 
